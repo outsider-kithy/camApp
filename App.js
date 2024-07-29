9e739483e762f5837e9d7f50e5525a4f8180affc
outsider-kithy <outsider.design.tech@gmail.com>
2024-07-29 10:03:03 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+
 
 export default function App() {
+  const cameraRef = useRef(null);
+  const [image, setImage] = useState(null);
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>アプリがカメラへのアクセスを求めています。</Text>
+        <Button onPress={requestPermission} title="許可" />
+      </View>
+    );
+  }
+
+  //写真を撮影する
+  const takePicture = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+    if (cameraRef.current) {
+      try {
+        const data = await cameraRef.current.takePictureAsync({options});
+        console.log(data);
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  //写真を保存する
+  const savePicture = async () => {
+    if (image) {
+      try {
+        const asset = await MediaLibrary.createAssetAsync(image);
+        alert('写真を保存しました。');
+        setImage(null);
+        console.log('保存完了');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView style={styles.camera} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+            <Button style={styles.button} title='撮影' onPress={takePicture}></Button>
+        </View>
+      </CameraView>
+      <Image source={{ uri: image }} style={styles.camera} />
+      <Button style={styles.button} title="保存" onPress={savePicture}></Button>
     </View>
   );
 }
@@ -13,8 +70,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+   	display:'block',
+	backgroundColor: 'blue',
+	color: 'white',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
---
+++ README.md
@@ -0,0 +1,16 @@
+# camApp
+React Native(Expo)を用いたカメラアプリの基本
+
+## Getting Start
+
+```shell
+npm install
+```
+
+## Version
+- Node.js : v22.0.0
+- expo : ~51.0.22,
+- expo-camera : ^15.0.14,
+- expo-media-library : ^16.0.4,
+- react : 18.2.0,
+- react-native : 0.74.3
